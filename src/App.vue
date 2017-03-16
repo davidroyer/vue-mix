215921960b8d58db847dafc7e283bdedc521ab5a
David Royer <droyer01@gmail.com>
2017-03-16 19:20:27 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="app">
+    <vue-editor></vue-editor>
+</div>
+</template>
+
+<script>
+import VueEditor from './VueEditor.vue';
+export default {
+    data() {
+        return {
+            name: 'App'
+        }
+    },
+    components: {
+        VueEditor
+    }
+}
+</script>
+
+<style lang="css">
+</style>
